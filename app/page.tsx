b7e0e253f39fea3a74c39fe1b716b680b7d0f0ce
sanseru <sanseru1@yahoo.com>
2024-10-04 10:54:18 +0700

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,12 @@ export default function Home() {
               Full Stack Software Developer
             </p>
             <p className="text-md max-w-2xl mx-auto leading-relaxed">
-            Experienced Full Stack Developer with 5+ years of expertise in building scalable web applications using PHP, Laravel, Node.js, and ERP systems. Skilled in .NET, Flutter, React Native, SCADA HMI, OPC DA, HDA, and UA. Continuously learning and applying cutting-edge technologies to deliver impactful solutions.            </p>
+              Experienced Full Stack Developer with 5+ years of expertise in
+              building scalable web applications using PHP, Laravel, Node.js,
+              and ERP systems. Skilled in .NET, Flutter, React Native, SCADA
+              HMI, OPC DA, HDA, and UA. Continuously learning and applying
+              cutting-edge technologies to deliver impactful solutions.{" "}
+            </p>
           </div>
           <div className="flex justify-center space-x-6">
             <a
@@ -114,11 +119,13 @@ export default function Home() {
             </div>
             <div className="lg:w-2/3 lg:pl-12 leading-loose">
               <p className="text-lg mb-6 leading-relaxed  text-gray-700">
-              I&apos;m Haris Lukman Hakim, a dedicated Full Stack Developer with over 5 years of experience
-          building high-performance web applications. My journey spans across industries,
-          where I&apos;ve developed scalable solutions using PHP, Laravel, Node.js, and ERP systems.
-          From modern medical administration platforms to warehouse management systems,
-          I bring a versatile and solution-oriented mindset to every project.
+                I&apos;m Haris Lukman Hakim, a dedicated Full Stack Developer
+                with over 5 years of experience building high-performance web
+                applications. My journey spans across industries, where
+                I&apos;ve developed scalable solutions using PHP, Laravel,
+                Node.js, and ERP systems. From modern medical administration
+                platforms to warehouse management systems, I bring a versatile
+                and solution-oriented mindset to every project.
               </p>
               <FeatureItem
                 icon={<Code size={24} className="text-blue-500" />}
@@ -137,12 +144,14 @@ export default function Home() {
               <FeatureItem
                 icon={<Book size={24} className="text-green-500" />}
                 title="Continuous Learning"
-                description="Committed to staying ahead of technology trends, I&apos;m constantly expanding my knowledge in microservices architecture,
+                description="Committed to staying ahead of technology trends, I'm constantly expanding my knowledge in microservices architecture,
           cloud computing, and automation technologies. This continuous learning fuels my ability to innovate and adapt in fast-evolving environments."
               />
               <p className="text-lg mt-8 font-semibold text-gray-800">
-              If you&apos;re looking for a forward-thinking developer who combines strong technical skills
-              with a passion for innovation, I would be excited to collaborate and help drive your project to success.
+                If you&apos;re looking for a forward-thinking developer who
+                combines strong technical skills with a passion for innovation,
+                I would be excited to collaborate and help drive your project to
+                success.
               </p>
             </div>
           </div>
@@ -166,9 +175,10 @@ export default function Home() {
                 PT. Kartika Bina Medikatama
               </h3>
               <p className="text-gray-600">
-                Led development of critical healthcare systems including MAS,
-                E-Commerce platforms, and CRM solutions. Spearheaded creation of
-                innovative HRIS, Chat Bot, and Covid-19 tracking applications.
+                Led a team of developers in building and maintaining internal
+                applications, ensuring high-quality code and timely delivery.
+                Managed project timelines, delegated tasks, and worked closely
+                with stakeholders to deliver business solutions.
               </p>
             </div>
             <div className="bg-white rounded-lg shadow-md p-6">
@@ -184,9 +194,10 @@ export default function Home() {
                 PT. Control System Arena Para Nusa
               </h3>
               <p className="text-gray-600">
-                Designed and implemented cutting-edge control systems.
-                Collaborated with cross-functional teams to develop and launch
-                innovative products, optimizing performance and user experience.
+                Designed, developed, and maintained software solutions for both
+                internal and client-facing applications. Collaborated with
+                cross-functional teams to gather requirements and deliver
+                high-quality software products
               </p>
             </div>
           </div>
@@ -199,12 +210,22 @@ export default function Home() {
           <div className="grid md:grid-cols-2 gap-8">
             <ProjectCard
               title="Medical Administration System (MAS)"
-              description="Developed a comprehensive healthcare management solution, including claims processing, EDC services, and integration with national providers."
+              description="Built a comprehensive platform for managing insurance policies, claims, and customer data, leading to better workflow management."
               company="PT. Kartika Bina Medikatama"
             />
             <ProjectCard
               title="HRIS & Payroll Integration"
-              description="Created a fully integrated HRIS system with payroll functionality, streamlining HR processes and improving efficiency."
+              description="Developed an HR system for managing employee information, attendance, and payroll. Improved operational efficiency and data accuracy."
+              company="PT. Kartika Bina Medikatama"
+            />
+            <ProjectCard
+              title="SAP Integration"
+              description="Automated business processes by integrating with SAP for seamless data flow and increased process automation."
+              company="PT. Kartika Bina Medikatama"
+            />
+            <ProjectCard
+              title="Helpline System"
+              description="Developed a customer support system to streamline issue tracking and resolution, enhancing customer satisfaction."
               company="PT. Kartika Bina Medikatama"
             />
             <ProjectCard
@@ -213,8 +234,43 @@ export default function Home() {
               company="PT. Kartika Bina Medikatama"
             />
             <ProjectCard
+              title="Courier System"
+              description="Streamlined logistics operations through a courier management system, optimizing delivery routes and reducing operational costs."
+              company="PT. Kartika Bina Medikatama"
+            />
+            <ProjectCard
+              title="Medical Checkup System"
+              description="Created a scheduling and management system for employee medical checkups, improving health monitoring and reporting."
+              company="PT. Kartika Bina Medikatama"
+            />
+
+            <ProjectCard
+              title="WhatsApp Notification System"
+              description="Integrated WhatsApp API for automated notifications, enhancing internal communications and process monitoring."
+              company="PT. Kartika Bina Medikatama"
+            />
+
+            <ProjectCard
               title="Contract Management System"
               description="Developed an advanced contract management solution for the FCLCS department, including budget tracking and client dashboards."
+              company="PT Control System Arena Paranusa"
+            />
+
+            <ProjectCard
+              title="Secure File Sharing System"
+              description="Developed a secure file sharing system using AWS S3 Storage for Pertamina PHONWJ, ensuring data integrity and accessibility."
+              company="PT Control System Arena Paranusa"
+            />
+
+            <ProjectCard
+              title="Principal App Terminal Manager Integration"
+              description="Created a service to integrate with the Principal App Terminal Manager for efficient order and contract creation."
+              company="PT Control System Arena Paranusa"
+            />
+
+            <ProjectCard
+              title="OPC Historical Data Access (HDA) Application"
+              description="Developed a web application for OPC Historical Data Access, facilitating data retrieval and analysis for decision-making."
               company="PT Control System Arena Paranusa"
             />
           </div>
