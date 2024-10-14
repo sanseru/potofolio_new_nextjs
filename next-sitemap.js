176b09959f72bb2d9b157a608d7eab267b8ea5e8
sanseru <sanseru1@yahoo.com>
2024-10-14 11:00:32 +0700

    Update next-sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://harislukman.my.id', // Ganti dengan URL situs Anda
+    generateRobotsTxt: true, // (Opsional)
+}
+
+module.exports = config
